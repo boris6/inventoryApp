1d433641986275a2116521f91f169fd51365efe0
Verica Radulovic <federica.e60@gmail.com>
2023-03-25 18:45:04 +0100

    Basic models

---
+++ InventoryApp/InventoryApp.Model/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using InventoryApp.Model.Enums;
+
+namespace InventoryApp.Model.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Code { get;set; }
+        public string Description { get; set; }
+        public int UnitOfMeasure { get; set; }
+        public decimal Price { get; set; }
+        public decimal NominalWeight { get; set; }
+        public User CreatedBy { get; set; }     
+    }
+}
