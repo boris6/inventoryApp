8e2af6c3211c5179aeeb6d795e3c8bd914769297
boris6 <sijanb@outlook.com>
2023-03-26 21:37:26 +0200

    Fix unit test

--- InventoryApp/InventoryApp.Test/IndexPageTest.cs
+++ InventoryApp/InventoryApp.Test/IndexPageTest.cs
@@ -9,32 +9,17 @@ namespace InventoryApp.Test;
 public class IndexPageTest
 {
     [Fact]
-    public async Task OnPost_IfInvalidModel_ReturnBadRequest()
+    public async Task OnPost_ThrowsException_WhenNoUser()
     {
         //arrange
 
         var contextBuilder = new DbContextOptionsBuilder<RepositoryContext>().UseSqlite("Data Source=:memory:;");
         var context = new RepositoryContext(contextBuilder.Options);
 
-        var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
-        {
-            new(ClaimTypes.NameIdentifier, "SomeValueHere"),
-            new Claim(ClaimTypes.Name, "gunnar@somecompany.com")
-            // other required and custom claims
-        }, "TestAuthentication"));
-
         var pageModel = new CreateModel(context);
-        pageModel.User.AddIdentity(new ClaimsIdentity(new[]
-        {
-            new(ClaimTypes.NameIdentifier, "SomeValueHere"),
-            new Claim(ClaimTypes.Name, "gunnar@somecompany.com")
-            // other required and custom claims
-        }));
+        
 
-        //
-        var result = await pageModel.OnPostAsync();
-
-        //assert
-        Assert.IsType<PageResult>(result);
+        //act and assert
+        await Assert.ThrowsAsync<NullReferenceException>(async () => await pageModel.OnPostAsync());
     }
 }
