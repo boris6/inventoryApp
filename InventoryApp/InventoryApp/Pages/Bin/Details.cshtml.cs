bbf0343830650c8a8ae853e5f8a5fd92e73de879
boris6 <sijanb@outlook.com>
2023-03-26 21:34:00 +0200

    Add products to Bin details

--- InventoryApp/InventoryApp/Pages/Bin/Details.cshtml.cs
+++ InventoryApp/InventoryApp/Pages/Bin/Details.cshtml.cs
@@ -1,43 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using InventoryApp.ContextFactory;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using InventoryApp.ContextFactory;
-using InventoryApp.Model.Models;
 
-namespace InventoryApp.Pages.Bin
+namespace InventoryApp.Pages.Bin;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly RepositoryContext _context;
+
+    public DetailsModel(RepositoryContext context)
     {
-        private readonly InventoryApp.ContextFactory.RepositoryContext _context;
+        _context = context;
+    }
 
-        public DetailsModel(InventoryApp.ContextFactory.RepositoryContext context)
-        {
-            _context = context;
-        }
+    public Model.Models.Bin Bin { get; set; } = default!;
+    public IList<Model.Models.Allocation> Allocations { get; set; } = default!;
 
-      public Model.Models.Bin Bin { get; set; } = default!; 
+    public async Task<IActionResult> OnGetAsync(Guid? id)
+    {
+        if (id == null || _context.Bins == null) return NotFound();
 
-        public async Task<IActionResult> OnGetAsync(Guid? id)
-        {
-            if (id == null || _context.Bins == null)
-            {
-                return NotFound();
-            }
-
-            var bin = await _context.Bins.FirstOrDefaultAsync(m => m.BinID == id);
-            if (bin == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Bin = bin;
-            }
-            return Page();
-        }
+        var bin = await _context.Bins.FirstOrDefaultAsync(m => m.BinID == id);
+        if (bin == null)
+            return NotFound();
+        var allocations = await _context.Allocations.Where(x => x.BinID == bin.BinID).Include(x => x.Product)
+            .ToListAsync();
+        Bin = bin;
+        Allocations = allocations;
+        return Page();
     }
 }
