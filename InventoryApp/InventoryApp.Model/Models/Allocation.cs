1abd72b5006b15b7d39fe79d002d7a07d488692f
Verica Radulovic <federica.e60@gmail.com>
2023-03-26 11:45:36 +0200

    Allocation and Bin models

---
+++ InventoryApp/InventoryApp.Model/Models/Allocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryApp.Model.Models
+{
+    public class Allocation
+    {
+        public Guid AllocationID { get; set; }
+        public Product Product { get; set; }
+        public Bin Bin { get; set; }
+        public decimal Quantity { get; set; }
+        public User? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
